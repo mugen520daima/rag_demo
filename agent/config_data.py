e419fb84b94fb1de7075f8cac6b1cfec776b7b85
Weller <chenweile02@baidu.com>
2026-05-19 20:23:31 +0800

    first day02

--- agent/config_data.py
+++ agent/config_data.py
@@ -1,2 +1,4 @@
 
-md5_path = "./md5.text"
+md5_path = "./md5.text" 
+collection_name = "rag"
+persist_directory = "./chroma_db"
--- agent/knowledge_base.py
+++ agent/knowledge_base.py
@@ -4,6 +4,8 @@
 import os
 from . import config_data as config
 import hashlib
+from langchain_chroma import Chroma
+from langchain_community.embeddings import DashScopeEmbeddings
 
 def check_md5(md5_str):
     """检查传入的md5字符串是否已经被处理过"""
@@ -34,9 +36,15 @@ def get_string_md5(input_str,encoding='utf-8'):
     return md5_hex
 
 class KnowledgeBaseService(object):
-    def __init__(self):
-        self.chroma = None
-        self.spliter = None
+    def __init__(self): 
+        os.makedirs(config.persist_directory, exist_ok=True)
+        self.chroma = Chroma(
+            collection_name=config.collection_name, #数据库的表名
+            embedding_function=DashScopeEmbeddings(model_name="text-embedding-v4") ,
+            persist_directory=config.persist_directory
+        ) #向量存储的实例 Chroma向量库对象
+        self.spliter = None #文本分割器的对象
 
     def upload_by_str(self,data,filename):
+        #将传入的字符串，进行向量化，存入向量化数据库中
         pass
